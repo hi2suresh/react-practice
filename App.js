fd76132b848895703c3970613a01525c0d930d27
hi2suresh <hi2suresh@gmail.com>
2025-08-10 05:54:55 +1000

    initial setup

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement('div', { id: 'parent' }, [
+  React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', {}, 'I am Child 1 H1'),
+    React.createElement('h2', { id: 'heading' }, 'I am Child 1 H2'),
+  ]),
+  React.createElement('div', { id: 'child2' }, [
+    React.createElement('h1', null, 'I am h1 from child2 '),
+    React.createElement('h2', { id: 'heading' }, 'I am h2 from child2'),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(parent);
+root.render(parent);
